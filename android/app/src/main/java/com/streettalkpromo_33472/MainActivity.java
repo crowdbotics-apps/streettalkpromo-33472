4e2f4bcbb9c4843d6fcaf7353715b4cadc4389b7
Crowdbotics <team@crowdbotics.com>
2022-02-09 17:30:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/streettalkpromo_33472/MainActivity.java
@@ -0,0 +1,15 @@
+package com.streettalkpromo_33472;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "streettalkpromo_33472";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "streettalkpromo_33472",
+  url: "https://streettalkpromo_33472.botics.co",
+  api: "https://streettalkpromo_33472.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
